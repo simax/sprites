9696ae3dda76f247bdbefff5a97852fc0eeadfac
Simon Lomax <simonlomax@ekm.com>
2024-03-02 15:31:56 +0000

    Add comment

--- main.go
+++ main.go
@@ -62,6 +62,7 @@ func moveImage(positionChan chan imagePos, win windowDimensions) {
 	}
 
 	for {
+		// Calculate the progress of the image movement
 		elapsed := time.Since(startTime)
 		progress := elapsed.Seconds() / totalDuration.Seconds()
 
